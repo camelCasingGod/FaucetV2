b6a354143f0fe3aebd7ff3ffd5564f5aa25a2cdd
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-21 22:54:46 -0700

    added explanation?

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,5 +2,12 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+
+    // this is a special function, called when you make a tx
+    // that doesn't specify function name to call
+
+    // External functions are part of the contract interface
+    // which means they can be called via contracts and other txs
+
     receive() external payable {}
 }
