8326e6474a2c1d611ad777ff78cba9343c9b435c
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-22 22:05:56 -0700

    Added a new function

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -3,11 +3,15 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-    // this is a special function, called when you make a tx
-    // that doesn't specify function name to call
-
-    // External functions are part of the contract interface
-    // which means they can be called via contracts and other txs
-
     receive() external payable {}
+
+    function addFunds() external payable {}
+
+    function justTesting() external pure {
+
+    }
+
+    // view - indicates that the function will not alter the storage state in any way
+    // pure - even more strict, indicating that it won't even read the storage state
+
 }
