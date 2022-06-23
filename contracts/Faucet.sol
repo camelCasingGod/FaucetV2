4b6b303edeead9be55e6a37a3ae8d296d3bc0350
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-22 22:08:49 -0700

    Added a new contract that doesn't have any gas fees

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -7,10 +7,11 @@ contract Faucet {
 
     function addFunds() external payable {}
 
-    function justTesting() external pure {
-
+    function justTesting() external pure returns(uint) {
+        return 2 + 2;
     }
 
+    // read-only calls, they will not need any gas fees
     // view - indicates that the function will not alter the storage state in any way
     // pure - even more strict, indicating that it won't even read the storage state
 
