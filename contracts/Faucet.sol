4b01797600188f85f2c4f3a3480e8c15f870e90a
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-23 22:58:39 -0700

    Changed the mapping to a [address -> boolean] mapping for making sure there's no duplicates

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -4,29 +4,30 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
 
     uint public numOfFunders;
-    mapping(uint => address) private funders;
-
-    // private -> can be accessible only within the smart contract
-    // internal -> can be accessible within smart contract and also derived smart contract
+    mapping(address => bool) private funders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        uint index = numOfFunders++;
-        funders[index] = msg.sender;
-    }
+        address funder = msg.sender;
 
-    function getAllFunders() external view returns (address[] memory) {
-        address[] memory _funders = new address[](numOfFunders);
-        for (uint8 n = 0; n < numOfFunders; n++) {
-            _funders[n] = funders[n];
+        if (!funders[funder]) {
+            numOfFunders++;
+            funders[funder] = true;
         }
-        return _funders;
     }
 
-    function getFunderAtIndex(uint8 index) external view returns (address) {
-        return funders[index];
-    }
+    // function getAllFunders() external view returns (address[] memory) {
+    //     address[] memory _funders = new address[](numOfFunders);
+    //     for (uint8 n = 0; n < numOfFunders; n++) {
+    //         _funders[n] = funders[n];
+    //     }
+    //     return _funders;
+    // }
+
+    // function getFunderAtIndex(uint8 index) external view returns (address) {
+    //     return funders[index];
+    // }
 
     // const instance = await Faucet.deployed()
     
