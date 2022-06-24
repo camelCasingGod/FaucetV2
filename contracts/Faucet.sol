8918dc649905c63d814a4944500f70da295b0c4c
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-23 19:16:39 -0700

    Added function to keep track of accounts that transfer funds

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -3,16 +3,12 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
+    address[] public funders;
+
     receive() external payable {}
 
-    function addFunds() external payable {}
-
-    function justTesting() external pure returns(uint) {
-        return 2 + 2;
+    function addFunds() external payable {
+        funders.push(msg.sender);
     }
-
-    // read-only calls, they will not need any gas fees
-    // view - indicates that the function will not alter the storage state in any way
-    // pure - even more strict, indicating that it won't even read the storage state
 
 }
