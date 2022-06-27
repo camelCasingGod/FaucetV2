135a246e5fb3e88bd170773d4fbe5676bb9787e1
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-26 18:40:51 -0700

    Used Metamask API to connect and fetch account

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      // access to: (via global API injection)
+      // window.ethereum
+      // window.web3
+
+      console.log(window.ethereum)
+      console.log(window.web3)
+    }
+
+    loadProvider()
+  }, [])
+
   return (
     <>
       <div className="faucet-wrapper">
@@ -9,6 +24,15 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong> ETH
           </div>
+          <button 
+            className="btn mr-2"
+            onClick={async () => {
+              const account = await window.ethereum.request({method: "eth_requestAccounts"})
+              console.log(account)
+            }}
+          >
+            Enable Ethereum
+          </button>
           <button className='btn mr-2'>Donate</button>
           <button className='btn'>Withdraw</button>
         </div>
