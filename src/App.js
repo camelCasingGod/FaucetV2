a8b11230811f0ce6127b5e05b57142f3f9f839d7
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-27 10:10:08 -0700

    imported web3, fixed issue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Web3 from "web3";
 
 function App() {
 
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+
   useEffect(() => {
     const loadProvider = async () => {
-      // access to: (via global API injection)
-      // window.ethereum
-      // window.web3
+      // with metamask we have an access to window.ethereum & to window.web3
+      // metamask injexts a global API into website
+      // this API allows websites to request users, accounts, read data to blockchain,
+      // sign messages and transactions
 
-      console.log(window.ethereum)
       console.log(window.web3)
+      console.log(window.ethereum)
+      let provider = null;
+
+      if (window.ethereum) {
+        provider = window.ethereum;
+
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User denied accounts access!")
+        }
+      }
+      else if (window.web3) {
+        provider = window.web3.currentProvider
+      }
+      else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545")
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
     }
 
     loadProvider()
   }, [])
+
+  console.log(web3Api.web3)
 
   return (
     <>
@@ -24,15 +55,6 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong> ETH
           </div>
-          <button 
-            className="btn mr-2"
-            onClick={async () => {
-              const account = await window.ethereum.request({method: "eth_requestAccounts"})
-              console.log(account)
-            }}
-          >
-            Enable Ethereum
-          </button>
           <button className='btn mr-2'>Donate</button>
           <button className='btn'>Withdraw</button>
         </div>
